870b35bf876a6a11d1f96ee59f8731354ea6f122
SakshiKadam2393 <167866473+SakshiKadam2393@users.noreply.github.com>
2025-03-24 17:51:14 +0530

    Add files via upload

---
+++ Spectacle Business Website/Spectacle Website 2/Login_Page.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "spectacle_business_website";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    $sql = "SELECT * FROM users WHERE username='$username'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows == 1) {
+        $row = $result->fetch_assoc();
+        if (password_verify($password, $row["password"])) {
+            $_SESSION["username"] = $row["username"];
+            $_SESSION["email"] = $row["email"];
+
+            header("Location: Home_Page.html");
+            exit();
+        } else {
+            echo "Invalid username or password.";
+        }
+    } else {
+        echo "Invalid username or password.";
+    }
+
+    $conn->close(); 
+}
+?>
